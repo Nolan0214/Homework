b5fe64602770cc999783e668d8f36ae177e92d91
Nolan0214 <b0966642707@gmail.com>
2025-11-13 18:50:10 +0800

    CH4

---
+++ 1110(p13)/1110(p13)/1110(p13).cpp
@@ -0,0 +1,33 @@
+﻿// 1110(p13).cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+double Power(double, int);
+void main(void)
+{
+    int k;
+    double Ans;
+    printf("計算3.5的k次方?請輸入k=");
+    scanf_s("%d", &k);  Ans = Power(3.5, k);
+    printf("3.5的%d次方=%f\n", k, Ans);
+    system("pause");
+}
+double Power(double X, int n)
+{
+    double PowerXn = 1;
+    for (int i = 1; i <= n; i++)
+        PowerXn = PowerXn * X;
+    return PowerXn;
+}
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
