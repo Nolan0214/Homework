8dab5e253150bd24e5eca87fc5fbb7f02b1b6932
Nolan0214 <b0966642707@gmail.com>
2025-11-26 22:06:53 +0800

    CH5

---
+++ 1122(p21)/1122(p21)/1122(p21).cpp
@@ -0,0 +1,51 @@
+﻿// 1122(p21).cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+void staticArrayInit(void);
+void automaticArrayInit(void);
+int main()
+{
+    printf("First call to each function:\n");
+    staticArrayInit();
+    automaticArrayInit();
+    printf("\n\nSecond call to each funcrtion:\n");
+    staticArrayInit();
+    automaticArrayInit();
+    printf("\n");
+    system("pause");
+    return 0;
+}
+void staticArrayInit(void)
+{
+    static int array1[3];
+    printf("\nValues on entering staticArrayInit:\n");
+    for (int i = 0; i <= 2; i++)
+        printf("array1[%d] = %d  ", i, array1[i]);
+    printf("\nValues on exiting staticArrayInit:\n");
+    for (int i = 0; i <= 2; i++)
+        printf("array1[%d] = %d  ", i, array1[i]+=5);
+}
+void automaticArrayInit(void)
+{
+    int array2[3] = { 1,2,3 };
+    printf("\n\nValues on entering automiticArrayInit:\n");
+    for (int i = 0; i <= 2; i++)
+        printf("array2[%d] = %d  ", i, array2[i]);
+    printf("\nValues on exiting automaticArrayInit:\n");
+    for (int i = 0; i <= 2; i++)
+        printf("array2[%d] = %d  ", i, array2[i] += 5);
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
