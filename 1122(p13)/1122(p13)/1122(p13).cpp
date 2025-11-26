cabf1c37348e21fd74956058dee4797837ae9ef5
Nolan0214 <b0966642707@gmail.com>
2025-11-26 21:57:20 +0800

    CH5

---
+++ 1122(p13)/1122(p13)/1122(p13).cpp
@@ -0,0 +1,43 @@
+﻿// 1122(p13).cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+void printArray(const int a[][3]);
+int main()
+{
+    int array1[2][3] = {{1,2,3},{4,5,6}};
+    int array2[2][3] = {1,2,3,4,5};
+    int array3[2][3] = {{1,2},{4}};
+    printf("Value in array1 by row are: \n");
+    printArray(array1);
+    printf("Value in array2 by row are: \n");
+    printArray(array2);
+    printf("Value in array3 by row are: \n");
+    printArray(array3);
+    system("pause");
+    return 0;
+}
+void printArray(const int a[][3])
+{
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
