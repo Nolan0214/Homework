6d491446be3e40dd7e7d5dec4750bacec64ff5d3
Nolan0214 <b0966642707@gmail.com>
2025-12-04 01:26:52 +0800

    CH5

---
+++ 1130(p25)/1130(p25)/1130(p25).cpp
@@ -0,0 +1,61 @@
+﻿// 1130(p25).cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[]);
+int main()
+{
+    const char *suit[4] = { "Hearts","Diamonds","Clubs","Spades" };
+    const char *    face[13] = { "Ace","Deuce","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King" };
+    int deck[4][13] = { 0 };
+    srand(time(0));
+    shuffle(deck);
+    deal(deck, face, suit);
+    system("pause");
+    return 0;
+}
+void shuffle(int wDeck[][13])
+{
+    int row, column, card;
+    for (card = 1; card <= 52; card++)
+    {
+        do
+        {
+            row = rand() % 4;
+            column = rand() % 13;
+        } while (wDeck[row][column] != 0);
+        wDeck[row][column] = card;
+    }
+}
+void deal(const int wDeck[][13], const char *wFace[], const char *wSuit[])
+{
+    int row, column, card;
+    for (card = 1; card <= 52; card++)
+    {
+        for (row = 0; row <= 3; row++)
+        {
+            for (column = 0; column <= 12; column++)
+            {
+                if (wDeck[row][column] == card)
+                {
+                    printf("%5s of %-8s%c", wFace[column], wSuit[row], card % 2 == 0 ? '\n' : '\t');
+                }
+            }
+        }
+    }
+}
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
